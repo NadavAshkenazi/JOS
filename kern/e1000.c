0bfe5b1f66e1c1ff6dea588592d8c4e6217cc2f6
NadavTAshkenazi <nadavash@campus.technion.ac.il>
2022-05-28 11:38:23 -0700

    lab6: finished ex4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,32 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
+#include <kern/picirq.h>
+#include <inc/trap.h>
 
 // LAB 6: Your driver code here
+
+ 
+
+volatile uint32_t *e1000Va;
+
+int e1000_attach(struct pci_func *pcif)
+{
+    pci_func_enable(pcif);
+
+    // // Init MMIO Region
+    e1000Va = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+
+    /*debug of mmio mapping */
+    
+    assert(0x80080783 == *(e1000Va + (BYTE_T0_ADDRESS(E1000_STATUS))));
+    cprintf("E1000 status: 0x%08x should be 0x80080783\n",*(e1000Va + (BYTE_T0_ADDRESS(E1000_STATUS))));
+    // // Set the interrupts channel
+    // irq_setmask_8259A(irq_mask_8259A & ~(1 << IRQ_E1000));
+
+    // e1000_load_mac_addr();
+    // e1000_tx_init();
+    // e1000_rx_init();
+
+    return 0;
+}
+
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,26 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include <kern/pci.h>
+
+// 82540EM (DESKTOP)  - in section 5.2 of intel's manual - page 109
+#define PCI_E1000_VENDOR 0x8086
+#define PCI_E1000_DEVICE 0x100E
+
+
+#define BYTE_T0_ADDRESS(BYTE_NUM) ((BYTE_NUM) / (4))
+
+//E1000 REGISTERS
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+#define E1000_EERD     0x00014  /* EEPROM Read - RW */
+
+
+
+
+int e1000_attach(struct pci_func *pcif); 
+// int e1000_transmit(struct PageInfo *payload_pp, size_t nbytes);
+// int e1000_receive(struct PageInfo **dest_pp);
+// void e1000_receive_interrupt_handler();
+
+
 #endif	// JOS_KERN_E1000_H
